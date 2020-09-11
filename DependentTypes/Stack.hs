f1d76b5e92fe754d2247e31f82f41b8d1ad24500
CristhianMotoche <cristhian.motoche@gmail.com>
2020-09-10 20:26:31 -0500

    Add SBool

---
+++ DependentTypes/Stack.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE ConstraintKinds        #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE RankNTypes             #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+
+
+
+import           Control.Monad.Trans.Writer
+import           Data.Constraint            (Dict (..))
+import           Data.Foldable              (for_)
+import           Data.Kind                  (Type)
+
